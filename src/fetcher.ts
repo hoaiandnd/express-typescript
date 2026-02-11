0eef9796c2fafdc1cc1383ee46c8853a6b407334
ngohoaian <ngohoaian01031@gmail.com>
2026-02-11 08:51:00 +0700

    update

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -85,7 +85,7 @@ export class Fetcher extends FetcherBase {
       console.log('Ket thuc cao du lieu')
       console.log('Tiep tuc voi trang ' + pageResult?.nextPage.nextPageIndex)
       appendToCSV(
-        './exports/long_an_150.csv',
+        './exports/long_an_162.csv',
         results.filter(r => r !== null && r !== undefined) as Record<string, unknown>[]
       )
       maxPagesToCrawl--
